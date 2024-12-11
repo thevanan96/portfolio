486e9d3aa734b4c77167798b7cfc4a8dd463b4b9
thevanan96 <jthevanan@gmail.com>
2024-12-11 21:42:10 +0530

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,7 @@ import { motion } from 'motion/react';
 
 const About = () => {
   return (
+    
     <div className="border-b border-neutral-900 pb-4">
       <h1 className="my-20 text-center text-4xl">
         About <span className="text-neutral-500">Me</span>
